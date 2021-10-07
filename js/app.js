1857d5ee4a27fe951711011dcc74f3ae4d071d85
QuentinOnFire <86719121+QuentinOnFire@users.noreply.github.com>
2021-10-07 12:53:38 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+document.getElementById("random").addEventListener("submit", function () {
+    var list = ["Quentin BORRAS", "Hugo FRANCOIS", "Calixte HUCHBOURG", "Louis BLIN",
+    "Louis HOSPITAL", "Timeo CHARUEL", "Valentin LIONET-FOULIGNY", "Marion DUTILLEUR",
+    "Kyra BERNI", "Alice ERNST", "Garance DEVIGNON", "Diane-Laure EVRARD",
+     "Lali BREUIL" ,"Antoine SAUTROT", "Tom GAUNY", "Thomas WINUM"]
+    var a = getRandomInt(15) + 1
+    document.getElementById("inject").innerHTML = `${list[a]}`
+});
