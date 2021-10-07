16482e299535e0fdab13a40feb877ed488038062
QuentinOnFire <86719121+QuentinOnFire@users.noreply.github.com>
2021-10-07 20:39:56 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
-
 document.getElementById("random").addEventListener("submit", function () {
     var list = ["Quentin BORRAS", "Hugo FRANCOIS", "Calixte HUCHBOURG", "Louis BLIN",
     "Louis HOSPITAL", "Timeo CHARUEL", "Valentin LIONET-FOULIGNY", "Marion DUTILLEUR",
     "Kyra BERNI", "Alice ERNST", "Garance DEVIGNON", "Diane-Laure EVRARD",
-     "Lali BREUIL" ,"Antoine SAUTROT", "Tom GAUNY", "Thomas WINUM"]
-    var a = getRandomInt(15) + 1
-    document.getElementById("inject").innerHTML = `${list[a]}`
+     "Lali BREUIL" ,"Antoine SAUTROT", "Tom GAUNY", "Thomas WINUM", "Faustine SCHREINER"]
+    var a = getRandomInt(15) + 1;
+    var html = `-> ${list[a]} <-`;
+    document.getElementById("inject").innerHTML = html
 });
