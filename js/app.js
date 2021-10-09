df06ec6d120b34b865cfb4c9583d0879a60f73a2
QuentinOnFire <86719121+QuentinOnFire@users.noreply.github.com>
2021-10-09 19:17:04 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ document.getElementById("random").addEventListener("submit", function () {
     var list = ["Quentin BORRAS", "Hugo FRANCOIS", "Calixte HUCHBOURG", "Louis BLIN",
     "Louis HOSPITAL", "Timeo CHARUEL", "Valentin LIONET-FOULIGNY", "Marion DUTILLEUR",
     "Kyra BERNI", "Alice ERNST", "Garance DEVIGNON", "Diane-Laure EVRARD",
-     "Lali BREUIL" ,"Antoine SAUTROT", "Tom GAUNY", "Thomas WINUM", "Faustine SCHREINER"]
+    "Antoine SAUTROT", "Tom GAUNY", "Thomas WINUM", "Faustine SCHREINER"]
     var a = getRandomInt(15) + 1;
     var html = `-> ${list[a]} <-`;
     var b = document.getElementById("center");
